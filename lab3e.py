75e46fff7fcb0822fbdfe957fd346a09cf654f16
Ishwinder Singh <isingh176@myseneca.ca>
2024-10-04 01:37:08 -0400

    Completed Lab3e

---
+++ lab3e.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Author ID- isingh176 
+# Create the list called "my_list" here, not within any function defined below.
+# This is a global object as it is defined outside any function.
+my_list = [100, 200, 300, 'six hundred']
+
+def give_list():
+    # Does not accept any arguments
+    # Returns all of items in the global object my_list unchanged
+    return my_list
+
+def give_first_item():
+    # Does not accept any arguments
+    # Returns the first item in the global object my_list as a string
+    return str(my_list[0]) 
+
+def give_first_and_last_item():
+    # Does not accept any arguments
+    # Returns a list that includes the first and last items in the global object my_list
+    return [my_list[0], my_list[-1]]
+
+def give_second_and_third_item():
+    # Does not accept any arguments
+    # Returns a list that includes the second and third items in the global object my_list
+    return [my_list[1], my_list[2]]
+
+if __name__ == '__main__':   # This section also referred to as a "main block"
+    print(give_list())                   # Output: [100, 200, 300, 'six hundred']
+    print(give_first_item())             # Output: 100
+    print(give_first_and_last_item())    # Output: [100, 'six hundred']
+    print(give_second_and_third_item())  # Output: [200, 300]
